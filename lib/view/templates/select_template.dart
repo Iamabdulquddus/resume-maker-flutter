b6182a63cae894bc4623b70919ccfa1b83b91e97
Abdul Quddus <abdulquddus7782@gmail.com>
2022-11-15 14:08:45 +0500

    abq

--- lib/view/templates/select_template.dart
+++ lib/view/templates/select_template.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resumemaker/constants/images.dart';
+import 'package:resumemaker/view/templates/template_name.dart';
 
 import 'all_templates/first_template.dart';
 import '../preview/preview.dart';
@@ -10,43 +11,34 @@ class SelectTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        crossAxisCount: 3,
-        shrinkWrap: true,
+      body: Column(
         children: [
-          SelectTemplateGridBox(),
+          ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: templateName.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Preview(
+                            template: templateName[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 100,
+                      color: Colors.black12,
+                    ),
+                  ),
+                );
+              }),
         ],
       ),
     );
   }
 }
-
-class SelectTemplateGridBox extends StatelessWidget {
-  const SelectTemplateGridBox({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => const Preview()));
-      },
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        height: 100,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white
-        ),
-        child: Center(
-          child: Image.asset(resumeBro),
-        ),
-      ),
-    );
-  }
-}
-
